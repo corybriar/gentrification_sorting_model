c13511691e8327491a98cf7eb1d31932241fa9b0
Cory Briar <cbriar@uoregon.edu>
2020-03-13 18:27:45 -0700

    Housing/Goods demands in hh sort

--- model_sim.jl
+++ model_sim.jl
@@ -108,6 +108,7 @@ end
 
 ϵ, εh, εw = shock_gen(para, nloc,JS)
 
+
 function Pℓ(para, γ, firms, prices, nloc)
     @unpack τ, M, S, ζ = para
     P_l = []
@@ -139,9 +140,12 @@ function household_sort(para, space, firms, prices, rents, wages, γ, n, εh, ε
     # Construct Pl
     Pl = Pℓ(para, γ, firms, prices, nloc)
     # Blank array to hold max for each city
-    maxm = zeros(IU + IS, 3, M)
+    maxm = zeros(IU + IS, 4, M)
+    # Blank matrix to hold π^e_s(ℓ'|ℓ)
+    πlpgl = []
     for m in 1:M
         # Blank matrix to hold value of ℓ,s combo in m
+        πm = zeros(nloc[m],nloc[m],2,S)
         Vsm = zeros(IU + IS, nloc[m], S)
         for e in E
             for s in 1:S
@@ -151,13 +155,14 @@ function household_sort(para, space, firms, prices, rents, wages, γ, n, εh, ε
                     .+ η.*log.(T .- n[e,s,m] .- γ[m])
                     )
                 # post residential sort work probabilities
-                πlpgl = exp.(VW) .* ((ones(1,nloc[m])*(exp.(VW))').^(-1))
+                πm[:,:,e,s] = exp.(VW) .* ((ones(1,nloc[m])*(exp.(VW))').^(-1))
+
                 # fill Vsm with utilities for each location under sector s
                 if e == 1
-                    Vsm[1:IU,:,s] = transpose((1/σh).*(diag(πlpgl .* transpose(VW)) .-
+                    Vsm[1:IU,:,s] = transpose((1/σh).*(diag(πm[:,:,e,s] .* transpose(VW)) .-
                         α[e].*log.(rents[m]) .- (1-α[e]).*log.(Pl[m]))) .+ εh[m][1:IU,:]
                 else
-                    Vsm[(IU+1):(IS + IU),:,s] = transpose((1/σh).*(diag(πlpgl .* transpose(VW)) .-
+                    Vsm[(IU+1):(IS + IU),:,s] = transpose((1/σh).*(diag(πm[:,:,e,s] .* transpose(VW)) .-
                         α[e].*rents[m] .- (1-α[e]).*Pl[m])) .+ εh[m][(IU+1):(IS + IU),:]
                 end
             end # s loop
@@ -165,6 +170,7 @@ function household_sort(para, space, firms, prices, rents, wages, γ, n, εh, ε
             if e == 1
                 # find maximizing choice ℓ in city m,s
                 maxm[1:IU,1,m]  = findmax(Vsm[1:IU,:,:], dims = (2,3))[1]
+                maxm[1:IU,4,m]  .= 1
                 for i in 1:I[e]
                     maxm[i,2,m] = findmax(Vsm, dims = (2,3))[2][i][2]
                     maxm[i,3,m] = findmax(Vsm, dims = (2,3))[2][i][3]
@@ -172,26 +178,62 @@ function household_sort(para, space, firms, prices, rents, wages, γ, n, εh, ε
             else
                 # find maximizing choice ℓ in city m,s
                 maxm[(IU+1):(IS + IU),1,m]  = findmax(Vsm[(IU+1):(IS + IU),:,:], dims = (2,3))[1]
+                maxm[(IU+1):(IS + IU),4,m] .= 2
                 for i in (I[e-1]):(I[e] + I[e-1])
                     maxm[i,2,m] = findmax(Vsm, dims = (2,3))[2][i][2]
                     maxm[i,3,m] = findmax(Vsm, dims = (2,3))[2][i][3]
                 end # i loop
             end # if statement
         end # e loop
+        # add to πlpgl array for use inb calculating incomes
+        push!(πlpgl,πm)
     end # m loop
     # Find utility maximizing city for each agent
     Mmax = zeros(IS + IU)
     for i in 1:(IS + IU)
         Mmax[i] = findmax(maxm[i,1,:])[2]
     end
+
     Mmax = Int.(Mmax)
-    Vmax = [maxm[1,2:3,Mmax]; Mmax']'
+    # Vector of best choices and income, [ℓ,s,e,m]
+    Vmax = [maxm[1,2:4,Mmax]; Mmax']'
+
+    pop = zeros(M)
+    for m in 1:M
+        pop[m] = count(i -> i == m, Vmax[:,4])
+    end # m loop
+    pop = Int.(pop)
+
+    # Compute household income's to infer demand for goods
+    HX = []
+    # HX[m] = [ℓ,ω,H,X]
+    for m in 1:M
+        # HXm = [ℓ,ωi,Hi,Xi]
+        HXm = zeros(0,4)
+        for e in E
+            for s in 1:S
+                # Filter down to only individuals with (s,e,m)
+                vmax = Vmax[Vmax[:,2] .== s,:]
+                vmax = vmax[vmax[:,3] .== e,:]
+                vmax = vmax[vmax[:,4] .== m,:]
+                vmax = Int.(vmax)
+                # Calculate expected wages
+                ωmse = transpose(πlpgl[m][:,vmax[:,1],e,s])*wages[m][:,e,s]
+                HXmse = hcat(vmax[:,1],
+                    ωmse,
+                    ωmse.*α[vmax[:,3]]./rents[m][vmax[:,1]] ,
+                    ωmse.*(1 .-α[vmax[:,3]])./Pl[m][vmax[:,1]])
+                HXm = [HXm ; HXmse]
+            end # s loop
+        end # e loop
+        push!(HX,HXm)
+    end # m loop
 
 end
 
 """
 MATRICES
-firms: [id, city, location, sector]
+firms: [city, location, sector]
 prices: [price]
 wages: w[m][ℓ,e,s]
 
